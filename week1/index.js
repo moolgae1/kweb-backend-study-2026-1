a5999ada0142841f2436bd57ea03b810190f4e4f
moolgae1 <moolgae6838@gmail.com>
2026-04-08 09:58:05 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/food", (req, res) => {
+    res.send("<h1>hi world</h1>");
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8080!')
+});
